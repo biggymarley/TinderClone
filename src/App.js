95cde05eb76c51baa0bca551d119ea966d31e4e3
biggymarley <afaragi@student.1337.ma>
2021-11-17 10:12:18 +0100

    adding images boxes

--- src/App.js
+++ src/App.js
@@ -1,17 +1,27 @@
 import React, { Suspense, useState } from "react";
 import Loading from "./content/Loading/Loading";
+import { createTheme, ThemeProvider } from '@mui/material/styles';
 export const LogginContext = React.createContext(false);
 function App() {
   const [Islogged, setIslogged] = useState(false);
   const Dashboard = React.lazy(() => Getdashboard());
   const HomeRoot = React.lazy(() => GetHomeRoot());
 
+const theme = createTheme({
+  palette: {
+    secondary: {
+      main: '#fe3f61',
+    },
+  },
+});
   return (
+    <ThemeProvider theme={theme}>
     <Suspense fallback={<Loading />}>
       <LogginContext.Provider value={{ Islogged, setIslogged }}>
         <div className="fadein">{Islogged ? <Dashboard /> : <Dashboard />}</div>
       </LogginContext.Provider>
     </Suspense>
+    </ThemeProvider>
   );
 }
 
@@ -28,6 +38,7 @@ const GetHomeRoot = async () => {
     setTimeout(
       () => resolve(import("./content/Home/HomeContent/HomeRoot")),
       2000
+
     );
   });
 };
--- src/content/Dashboard/CreationStepper.js
+++ src/content/Dashboard/CreationStepper.js
@@ -1,8 +1,16 @@
-import { Box, Checkbox, Container, Grid, Stack } from "@mui/material";
+import {
+  Box,
+  Button,
+  Checkbox,
+  Container,
+  Grid,
+  Stack,
+  TextField,
+} from "@mui/material";
 import React from "react";
 import { Logo } from "../Home/ContentFactory/IconFactory";
 import { Typography } from "@mui/material";
-
+import { FormikProvider, useFormik, FormikContext } from "formik";
 export default function CreationStepper() {
   return (
     <>
@@ -24,36 +32,171 @@ const SimpleHeader = () => {
 };
 
 const Stepper = () => {
+  const formik = useFormik({
+    initialValues: {
+      Gender: "",
+      SexualPreferences: "",
+      Biografy: "",
+    },
+
+    onSubmit: (values) => {
+      alert(JSON.stringify(values, null, 2));
+    },
+  });
   return (
     <Container maxWidth="lg" sx={classes.StepperRppt}>
       <Typography variant="h3" sx={classes.TitleHead}>
         Create Account
       </Typography>
-      <Grid container spacing={2}>
-        <Grid item>
-          <Gender />
-        </Grid>
-        <Grid item></Grid>
-        <Grid></Grid>
-      </Grid>
+      <FormikProvider value={formik}>
+        <form onSubmit={formik.handleSubmit}>
+          <Grid container spacing={2} rowSpacing={5} sx={classes.Gridroot}>
+            <Grid item xs={12} sm={6}>
+              <Gender />
+            </Grid>
+            <Grid item xs={12} sm={6}>
+              <SexualPreferences />
+            </Grid>
+            <Grid item xs={12}>
+              <Biografy />
+            </Grid>
+            <Grid item xs={12}>
+              <UploadImages />
+            </Grid>
+            <Grid item xs={12}>
+              <Button type="submit" variant="contained" color="secondary">
+                submit
+              </Button>
+            </Grid>
+          </Grid>
+        </form>
+      </FormikProvider>
     </Container>
   );
 };
-const Gender = () => {
+const UploadImages = () => {
   return (
-    <Stack direction="row" spacing={2}>
-      <div>
-        <input type="radio" name="s-size" id="Woman" className="hiddenChebox" />
-        <label htmlFor="Woman" className="VisibleChebox">
-          Woman
-        </label>
-      </div>
-      <div>
-        <input type="radio" name="s-size" id="Man" className="hiddenChebox" />
-        <label htmlFor="Man" className="VisibleChebox">
-          Man
-        </label>
-      </div>
+    <Box sx={classes.UploadRoot}>
+      <Grid container spacing={5} justifyContent='center' alignItems="center">
+        <Grid item>
+          <Box sx={classes.ImgBox}></Box>
+        </Grid>
+        <Grid item>
+          <Box sx={classes.ImgBox}></Box>
+        </Grid>
+        <Grid item>
+          <Box sx={classes.ImgBox}></Box>
+        </Grid>
+        <Grid item>
+          <Box sx={classes.ImgBox}></Box>
+        </Grid>
+        <Grid item>
+          <Box sx={classes.ImgBox}></Box>
+        </Grid>
+      </Grid>
+    </Box>
+  );
+};
+const Biografy = () => {
+  const formik = React.useContext(FormikContext);
+  return (
+    <Stack direction="column" spacing={1} sx={{ width: "100%" }}>
+      <Typography sx={classes.label}>Biografy</Typography>
+      <TextField
+        sx={classes.TextField}
+        rows={4}
+        multiline
+        name="Biografy"
+        value={formik.values.Biografy}
+        onChange={formik.handleChange}
+        fullWidth
+        required
+      />
+    </Stack>
+  );
+};
+const Gender = () => {
+  const formik = React.useContext(FormikContext);
+  return (
+    <Stack direction="column" spacing={3}>
+      <Typography sx={classes.label}>Gender</Typography>
+      <Stack direction="row" spacing={2}>
+        <div>
+          <input
+            type="radio"
+            id="Woman"
+            className="hiddenChebox"
+            value={"Woman"}
+            name="Gender"
+            onChange={formik.handleChange}
+            checked={formik?.values?.Gender === "Woman" ? true : false}
+          />
+          <label htmlFor="Woman" className="VisibleChebox">
+
+             Woman
+
+          </label>
+        </div>
+        <div>
+          <input
+            type="radio"
+            id="Man"
+            className="hiddenChebox"
+            value={"Man"}
+            name="Gender"
+            onChange={formik.handleChange}
+            checked={formik?.values?.Gender === "Man" ? true : false}
+          />
+          <label htmlFor="Man" className="VisibleChebox">
+            Man
+          </label>
+        </div>
+      </Stack>
+    </Stack>
+  );
+};
+
+const SexualPreferences = () => {
+  const formik = React.useContext(FormikContext);
+  return (
+    <Stack direction="column" spacing={3}>
+      <Typography sx={classes.label}>Sexual preferences</Typography>
+      <Stack direction="row" spacing={2}>
+        <div>
+          <input
+            type="radio"
+            id="Heterosexual"
+            className="hiddenChebox"
+            value={"Heterosexual"}
+            name="SexualPreferences"
+            onChange={formik.handleChange}
+            checked={
+              formik?.values?.SexualPreferences === "Heterosexual"
+                ? true
+                : false
+            }
+          />
+          <label htmlFor="Heterosexual" className="VisibleChebox">
+            Heterosexual
+          </label>
+        </div>
+        <div>
+          <input
+            type="radio"
+            id="Bi-sexual"
+            className="hiddenChebox"
+            value={"Bisexual"}
+            name="SexualPreferences"
+            onChange={formik.handleChange}
+            checked={
+              formik?.values?.SexualPreferences === "Bisexual" ? true : false
+            }
+          />
+          <label htmlFor="Bi-sexual" className="VisibleChebox">
+            Bisexual
+          </label>
+        </div>
+      </Stack>
     </Stack>
   );
 };
@@ -62,6 +205,18 @@ const classes = {
   HeaderRoot: {
     p: { xs: ".5rem", sm: "1rem" },
     borderBottom: "1px solid #0000001a",
+  },
+  UploadRoot: {
+    width: "calc(100%)",
+    backgroundColor: "#f0f2f430",
+    borderRadius: "10px",
+    p: ".5rem",
+  },
+  ImgBox:{
+    minHeight:"12rem",
+    minWidth: "9rem",
+    border: "1px solid #505965",
+    borderRadius:"10px"
   },
   StepperRppt: {
     display: "flex",
@@ -82,8 +237,46 @@ const classes = {
   TitleHead: {
     fontFamily: "Roboto",
     fontStyle: "italic",
-    fontSize: "clamp(1.5rem, 2.5vw, 2rem)",
+    fontSize: "clamp(1.2rem, 2.5vw, 2rem)",
     textTransform: "uppercase",
     paddingBottom: { xs: "4rem", sm: "5rem" },
+  },
+  label: {
+    fontFamily: "Roboto",
+    fontSize: "1rem",
+    textTransform: "capitalize",
+    fontWeight: 500,
+  },
+  Gridroot: {
+    "& > .MuiGrid-item": {
+      display: "flex",
+      justifyContent:{ xs: "center", sm: "flex-start" },
+      alignItems: { xs: "center", sm: "flex-start" },
+    },
+  },
+  TextField: {
+    borderRadius: "10px",
+    paddingBottom: { xs: "1rem", sm: "0" },
+    "& textarea": {
+      borderRadius: "10px !important",
+    },
+    "& textarea:valid ~ fieldset": {
+      borderColor: "secondary.main",
+      borderWidth: 2,
+    },
+    "& label.Mui-focused": {
+      color: "secondary.main",
+    },
+    "& .MuiInput-underline:after": {
+      borderBottomColor: "#505965",
+    },
+    "& .MuiOutlinedInput-root": {
+      "&.Mui-focused fieldset": {
+        borderColor: "secondary.main",
+      },
+    },
+    "& fieldset": {
+      borderRadius: "10px",
+    },
   },
 };
