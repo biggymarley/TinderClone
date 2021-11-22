dc2c8f9b24e35c213edbd1f8a7a03bc6ab8f320c
biggymarley <afaragi@student.1337.ma>
2021-11-22 03:11:02 +0100

    building user Detailed infos

--- src/content/Dashboard/MainArea/HomeArea.js
+++ src/content/Dashboard/MainArea/HomeArea.js
@@ -1,85 +1,215 @@
-import { Box, IconButton, Stack, Typography } from "@mui/material";
-import React, { useState } from "react";
+import {
+  Avatar,
+  Box,
+  Button,
+  Divider,
+  Fade,
+  IconButton,
+  Stack,
+  Typography,
+} from "@mui/material";
+import React, { useEffect, useState } from "react";
 import ClearRoundedIcon from "@mui/icons-material/ClearRounded";
 import FavoriteRoundedIcon from "@mui/icons-material/FavoriteRounded";
 import FiberManualRecordIcon from "@mui/icons-material/FiberManualRecord";
 import LocationOnOutlinedIcon from "@mui/icons-material/LocationOnOutlined";
 import InfoRoundedIcon from "@mui/icons-material/InfoRounded";
+import ArrowDownwardRoundedIcon from "@mui/icons-material/ArrowDownwardRounded";
 export default function HomeArea() {
   const [pics, setpics] = useState(1);
+  const [searching, setsearching] = useState(false);
 
+  useEffect(() => {
+    setTimeout(() => {
+      setsearching(true);
+    }, 0);
+  }, []);
   const inctement = () => {
     if (pics >= 99) setpics(1);
     else setpics(pics + 1);
   };
   return (
     <Box sx={classes.root}>
-      <Box sx={classes.AvailableUserBox}>
-        <Box sx={classes.AvailableUserImages}>
-          <img
-            src={`https://randomuser.me/api/portraits/women/${pics}.jpg`}
-            alt="user-gallery"
-            style={classes.UserImage}
-          />
-          <InfoStack />
-          <ButtonsStack inctement={inctement} />
-          <Box sx={classes.BottomShadow}></Box>
-        </Box>
-      </Box>
+      {!searching ? (
+        <NoMatchingProfiles />
+      ) : (
+        <AvailableProfiles inctement={inctement} pics={pics} />
+      )}
     </Box>
   );
 }
 
-const InfoStack = ({ inctement }) => {
+const NoMatchingProfiles = () => {
+  return (
+    <Box sx={classes.NoMatchingRoot}>
+      <Box sx={classes.userLogo}>
+        <Avatar
+          alt="profile-pic"
+          src="https://picsum.photos/200/300"
+          sx={classes.userAvatar}
+        />
+        <Box sx={classes.Radar} className="Radar" />
+      </Box>
+    </Box>
+  );
+};
+
+const AvailableProfiles = ({ inctement, pics }) => {
+  const [openPinfo, setopenPinfo] = useState(false);
+
+  const ToggleOpen = () => {
+    setopenPinfo(!openPinfo);
+    console.log(openPinfo);
+  };
+
+  return (
+    <Box sx={classes.AvailableUserBox}>
+      <Fade in={!openPinfo}>
+        <div style={{ width: "100%", height: "100%", position: "absolute" }}>
+          <PreviewProfile
+            inctement={inctement}
+            pics={pics}
+            ToggleOpen={ToggleOpen}
+          />
+        </div>
+      </Fade>
+      <Fade in={openPinfo}>
+        <div style={{ height: "100%" }}>
+          <DetailedUserInfos
+            pics={pics}
+            ToggleOpen={ToggleOpen}
+            inctement={inctement}
+          />
+        </div>
+      </Fade>
+    </Box>
+  );
+};
+
+const DetailedUserInfos = ({ pics, ToggleOpen, inctement }) => {
+  return (
+    <Box sx={classes.AvailableUserInfos} className="hidescrollbar">
+      <Box sx={classes.imgwraper}>
+        <img
+          src={`https://randomuser.me/api/portraits/women/${pics}.jpg`}
+          alt="user-gallery"
+          style={classes.UserDetailedImage}
+        />
+        <Box sx={classes.BottomShadowdetailed} />
+        <IconButton sx={classes.DownButton} onClick={ToggleOpen}>
+          <ArrowDownwardRoundedIcon />
+        </IconButton>
+      </Box>
+      <InfoStack color={true} noStatus={true} />
+      <Divider />
+      <Box display="grid" placeItems="center" minHeight="60px">
+        <Button sx={classes.reportButton}>Report Name</Button>
+      </Box>
+      <Divider />
+      <Box minHeight="30px" />
+      <ButtonsStack inctement={inctement} contained={true} />
+    </Box>
+  );
+};
+
+const PreviewProfile = ({ inctement, pics, ToggleOpen }) => {
+  return (
+    <Box sx={classes.AvailableUserImages}>
+      <img
+        src={`https://randomuser.me/api/portraits/women/${pics}.jpg`}
+        alt="user-gallery"
+        style={classes.UserImage}
+      />
+
+      <InfoStack ToggleOpen={ToggleOpen} />
+      <ButtonsStack inctement={inctement} />
+      <Box sx={classes.BottomShadow}></Box>
+    </Box>
+  );
+};
+
+const InfoStack = ({ inctement, ToggleOpen, color, noStatus }) => {
   return (
     <Stack
       direction="row"
       justifyContent="space-between"
       sx={classes.InfoStack}
+      style={noStatus ? { position: "static" } : {}}
     >
       <Stack direction="column" spacing={1}>
         <Box>
           <Stack direction="row" spacing={1} alignItems="center">
-            <Typography variant="span" sx={classes.userName}>
+            <Typography
+              variant="span"
+              sx={classes.userName}
+              style={color ? { color: "#000000" } : {}}
+            >
               Name
             </Typography>
-            <Typography variant="span" sx={classes.age}>
+            <Typography
+              variant="span"
+              sx={classes.age}
+              style={color ? { color: "#000000" } : {}}
+            >
               26
             </Typography>
           </Stack>
-          <Stack direction="row" spacing={1} alignItems="center">
-            <FiberManualRecordIcon
-              sx={classes.connectionStatus}
-              style={{ color: "#2df187" }}
-            />
-            <Typography sx={classes.connectionStatus}>
-              recently active
-            </Typography>
-          </Stack>
+          {noStatus ? (
+            false
+          ) : (
+            <Stack direction="row" spacing={1} alignItems="center">
+              <FiberManualRecordIcon
+                sx={classes.connectionStatus}
+                style={{ color: "#2df187" }}
+              />
+              <Typography sx={classes.connectionStatus}>
+                recently active
+              </Typography>
+            </Stack>
+          )}
         </Box>
         <Stack direction="row" spacing={1} alignItems="center">
-          <LocationOnOutlinedIcon sx={classes.mailes} />
-          <Typography sx={classes.mailes}>121 Kilometres away</Typography>
+          <LocationOnOutlinedIcon
+            sx={classes.mailes}
+            style={color ? { color: "#000000", opacity: ".5" } : {}}
+          />
+          <Typography
+            sx={classes.mailes}
+            style={color ? { color: "#000000", opacity: ".5" } : {}}
+          >
+            121 Kilometres away
+          </Typography>
         </Stack>
       </Stack>
-      <IconButton sx={classes.infoButton} onClick={()=>console.log("dd")}>
-        <InfoRoundedIcon sx={{fontSize:"2rem"}}/>
-      </IconButton>
+      {noStatus ? (
+        false
+      ) : (
+        <IconButton sx={classes.infoButton} onClick={ToggleOpen}>
+          <InfoRoundedIcon sx={{ fontSize: "2rem" }} />
+        </IconButton>
+      )}
     </Stack>
   );
 };
 
-const ButtonsStack = ({ inctement }) => {
+const ButtonsStack = ({ inctement, contained }) => {
   return (
     <Stack
       direction="row"
       sx={classes.ButtonStack}
       justifyContent="space-evenly"
+      style={contained ? { position: "sticky" } : {}}
     >
-      <IconButton sx={classes.crossButton} onClick={inctement}>
+      <IconButton
+        sx={contained ? classes.contained : classes.crossButton}
+        onClick={inctement}
+      >
         <ClearRoundedIcon sx={classes.cross} />
       </IconButton>
-      <IconButton sx={classes.hearthButton} onClick={inctement}>
+      <IconButton
+        sx={contained ? classes.contained : classes.hearthButton}
+        onClick={inctement}
+      >
         <FavoriteRoundedIcon sx={classes.hearth} />
       </IconButton>
     </Stack>
@@ -92,12 +222,59 @@ const classes = {
     display: "grid",
     placeItems: "center",
   },
+  DownButton: {
+    position: "absolute",
+    background: "linear-gradient(225deg,#ff6036,#fd267a)",
+    color: "#FFFFFF",
+    bottom: "-15px",
+    right: "20px",
+  },
+  reportButton: {
+    color: "#000000",
+    opacity: 0.3,
+    fontFamily: "Nova",
+    borderRadius: "0",
+    transition: "opacity .5s ease",
+    "&:hover": {
+      opacity: 0.5,
+    },
+  },
   AvailableUserBox: {
+    position: "relative",
     width: "370px",
     height: "640px",
     borderRadius: "10px",
     boxShadow:
       "rgba(60, 64, 67, 0.3) 0px 1px 2px 0px, rgba(60, 64, 67, 0.15) 0px 2px 6px 2px",
+  },
+  NoMatchingRoot: {
+    position: "relative",
+    width: "15rem",
+    height: "15rem",
+    display: "grid",
+    placeItems: "center",
+  },
+  userAvatar: {
+    width: "7rem",
+    height: "7rem",
+    border: "2px solid #FFFFFF",
+    zIndex: "2",
+  },
+  Radar: {
+    position: "absolute",
+    bottom: 0,
+    right: 0,
+    left: 0,
+    top: 0,
+    margin: "auto",
+    width: "1rem",
+    height: "1rem",
+    borderRadius: "50%",
+    color: "secondary.main",
+    border: ".3rem solid ",
+    zIndex: "1",
+    opacity: 1,
+    backgroundColor: "#fe3f618c",
   },
   AvailableUserImages: {
     width: "100%",
@@ -105,11 +282,34 @@ const classes = {
     borderRadius: "10px",
     position: "relative",
   },
+  AvailableUserInfos: {
+    backgroundColor: "#FFFFFF",
+    borderRadius: "10px",
+    position: "relative",
+    width: "100%",
+    height: "100%",
+    overflow: "clip auto",
+  },
   UserImage: {
     width: "100%",
     height: "100%",
     objectFit: "cover",
     borderRadius: "10px",
+  },
+  UserDetailedImage: {
+    width: "100%",
+    objectFit: "cover",
+    borderRadius: "10px 10px 0 0",
+    height: "100%",
+  },
+  imgwraper: { width: "100%", position: "relative", height: "70%" },
+  BottomShadowdetailed: {
+    position: "absolute",
+    top: 0,
+    borderRadius: "10px 10px 0 0",
+    width: "100%",
+    height: "100%",
+    boxShadow: "rgba(0, 0, 0, 0.70) 0px -70px 36px -28px inset",
   },
   BottomShadow: {
     position: "absolute",
@@ -117,7 +317,7 @@ const classes = {
     borderRadius: "10px",
     width: "100%",
     height: "100%",
-    boxShadow: "inset 0px -12rem 5rem -60px #000000",
+    boxShadow: "inset 0px -15rem 5rem -70px #000000",
   },
   ButtonStack: {
     position: "absolute",
@@ -129,13 +329,17 @@ const classes = {
     position: "absolute",
     width: "100%",
     p: "1rem",
-    bottom: "15%",
+    bottom: "12%",
     zIndex: 3,
     color: "#FFFFFF",
   },
   crossButton: {
     color: "secondary.main",
     border: "1px solid ",
+  },
+  contained: {
+    boxShadow: "rgba(0, 0, 0, 0.24) 0px 3px 8px",
+    backgroundColor: "#FFFFFF !important",
   },
   hearthButton: {
     color: "#21d07c",
@@ -155,6 +359,9 @@ const classes = {
     width: "2.5rem",
     height: "2.5rem",
     transition: "all .2s",
+    "&:hover": {
+      transform: "scale(1.08)",
+    },
   },
   userName: {
     fontFamily: "Nova",
